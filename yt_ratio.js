65fc0135ac566af41a679f2982249b3c2c2108fa
Ben Bradford <bradfordium@gmail.com>
2021-11-26 18:16:29 -0600

    refactor code

--- yt_ratio.js
+++ yt_ratio.js
@@ -1,22 +1,23 @@
 // Parses view count and likes count for YouTube videos
 
-var countSelector = "#info #info-text #count"
-var likesSelector = "#menu-container #top-level-buttons-computed #text"
+var viewsSelector = "#info #info-text #count";
+var likesSelector = "#menu-container #top-level-buttons-computed #text";
+var likeButtonSelector = "#menu-container #top-level-buttons-computed #button #button";
+var ratioElemId = "yt-likes-ratio";
 
 function elementReady(selector) {
     return new Promise((resolve, reject) => {
         let el = document.querySelector(selector);
         if (el) {
-          resolve(el); 
-          return
+          resolve(el);
+          return;
         }
         new MutationObserver((mutationRecords, observer) => {
             Array.from(document.querySelectorAll(selector)).forEach((element) => {
                 resolve(element);
                 observer.disconnect();
             });
-        })
-        .observe(document.documentElement, {
+        }).observe(document.documentElement, {
             childList: true,
             subtree: true
         });
@@ -26,37 +27,32 @@ function elementReady(selector) {
 function parseLikes() {
     try {
         // Remove any existing likes ratio element
-        if (document.contains(document.getElementById("yt-likes-ratio"))) {
-            document.getElementById("yt-likes-ratio").remove();
-        }
+        if (document.contains(document.getElementById(ratioElemId))) document.getElementById(ratioElemId).remove();
 
         // Get and parse the view count element
-        var viewsContainer = document.querySelector(countSelector)
-        var viewsElem = viewsContainer.getElementsByClassName("view-count")[0]
-        var viewsText = viewsElem.innerText.split(" ")[0].replace(/,/g, '');
-        var views = parseInt(viewsText);
+        let viewsContainer = document.querySelector(viewsSelector);
+        let viewsElem = viewsContainer.getElementsByClassName("view-count")[0];
+        let viewsText = viewsElem.innerText.split(" ")[0].replace(/,/g, '');
+        let views = parseInt(viewsText);
         // console.log(views + " views");
 
         // Get and parse the likes count element
-        likesElem = document.querySelector(likesSelector)
-        var likesText = likesElem.ariaLabel;
-        likesText = likesText.split(" ")[0].replace(/,/g, '');
-        likesText = likesText.replace("K", "000").replace("M", "000000").replace("B", "000000000");
-        if (likesText.includes(".")) {
-            likesText = likesText.substring(0, likesText.length - 1);
-        }
-        var likes = parseInt(likesText);
+        let likesElem = document.querySelector(likesSelector);
+        let likesText = likesElem.ariaLabel.split(" ")[0].replace(/,/g, '').replace("K", "000").replace("M", "000000").replace("B", "000000000");
+        if (likesText.includes(".")) likesText = likesText.substring(0, likesText.length - 1);
+        let likes = parseInt(likesText);
         // console.log(likes + " likes");
 
         // Create ratio
-        var ratio = likes * 1.0 / views;
-        var ratioText = "(" + (ratio * 100).toFixed(2) + "% liked)";
+        let ratio = likes * 1.0 / views;
+        let ratioText = "(" + (ratio * 100).toFixed(2) + "% liked)";
         // console.log(ratioText);
 
         // Create element with ratio text in it
         const ratioElem = document.createElement("span");
-        ratioElem.id = "yt-likes-ratio";
+        ratioElem.id = ratioElemId;
         ratioElem.style.marginLeft = "0.5em";
+
         const ratioContent = document.createTextNode(ratioText);
         ratioElem.appendChild(ratioContent);
         likesElem.insertBefore(ratioElem, null);
@@ -65,12 +61,13 @@ function parseLikes() {
     }
 }
 
-// Observe video change or like button press and refresh the count
-function createObservers() {
-    const observer = new MutationObserver(() => { parseLikes(); });
-    observer.observe(document.querySelector("title"), { attributes: true });
-    observer.observe(document.querySelector("#menu-container #top-level-buttons-computed #button #button"), { attributes: true });
-}
+const observer = new MutationObserver(() => { parseLikes() });
 
-// Wait for both elements to be ready on the page
-(elementReady(countSelector) && elementReady(likesSelector)).then(() => { parseLikes(); createObservers(); })
+// Create an observer on the page title to trigger the parse
+elementReady("title").then(() => { observer.observe(document.querySelector("title"), { attributes: true }) });
+
+// Create observer on the like button to re-parse
+elementReady(likeButtonSelector).then(() => { observer.observe(document.querySelector(likeButtonSelector), { attributes: true }) });
+
+// Wait for vew and likes elements to be ready on the page to run the parse
+(elementReady(viewsSelector) && elementReady(likesSelector)).then(() => { parseLikes() });
