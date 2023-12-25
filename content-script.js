4d8006d694fb94c828817abb5359d6bb8a7b693e
Ben Bradford <bradfordium@gmail.com>
2023-12-24 23:58:20 -0600

    correctly refresh on view count update

--- content-script.js
+++ content-script.js
@@ -1,21 +1,20 @@
 // --- Parses view count and likes count for YouTube videos --- //
 
+// stops execution on error
 let hasNoErrors = true;
 
 // element definitions
 const ratioElemId = 'yt-likes-ratio';
 
-// where to look for view counts, in .innerText
-const viewCountSelector = "#count > ytd-video-view-count-renderer > span.view-count.style-scope.ytd-video-view-count-renderer"
+// where to look for view counts, in .innerText (initial) or .ariaLabel (primary)
+const initialViewCountSelector = "#count > ytd-video-view-count-renderer > span.view-count.style-scope.ytd-video-view-count-renderer"
+const primaryViewCountSelector = "#view-count"
 
 // where to look for likes count in .ariaLabel
 const likeButtonSelector = '#top-level-buttons-computed > segmented-like-dislike-button-view-model > yt-smartimation > div > div > like-button-view-model > toggle-button-view-model > button';
 
-// container to watch for removal of the likes ratio element to trigger re-creating it
-const removalObserverSelector = '#top-level-buttons-computed';
-
-// insert the ratio element after this one
-const ratioElemAnchor = '#top-level-buttons-computed > segmented-like-dislike-button-view-model';
+// container for ratio element and to trigger re-creating it
+const ratioElemContainer = '#top-level-buttons-computed';
 
 // query selector alias
 const $ = document.querySelector.bind(document);
@@ -39,41 +38,48 @@ const elementReady = (selector) => {
   });
 }
 
-// convert text count to number
-const parseCount = (str) => {
-  str = str.replace(/,/g, '').replace('K', '000').replace('M', '000000').replace('B', '000000000');
-  if (str.includes('.')) str = str.substring(0, str.length - 1);
-  let count = parseInt(str);
-  count = isNaN(count) ? 0 : count;
-  return count;
-}
-
 // format number with thousands commas
 const fmtNumber = (num) => {
   if (typeof num != 'number') return num;
   return num.toLocaleString('en-US', {style: 'decimal', maximumFractionDigits: 0});
 }
 
+// convert text count to number
+const parseCount = (str) => {
+  str = str.replace(/,/g, '').replace('K', '000').replace('M', '000000').replace('B', '000000000');
+  if (str.includes('.')) str = str.substring(0, str.length - 1);
+  return parseInt(str);
+}
+
+// parse the view count, expected format '93,123 views'
+const getViewCount = () => {
+  const el1 = $(initialViewCountSelector);
+  const el2 = $(primaryViewCountSelector);
+  let viewsText = '';
+  if (el1) viewsText = el1.innerText.split(' ')[0];
+  if (el2 && el2.ariaLabel != '') viewsText = el2.ariaLabel.split(' ')[0];
+  return parseCount(viewsText);
+}
+
+// Parse the likes count, expected format 'like this video along with 3,239 other people'
+const getLikesCount = () => {
+  const el = $(likeButtonSelector);
+  let likesText = el ? el.ariaLabel.split('with ')[1].split(' ')[0] : '';
+  return parseCount(likesText);
+}
+
 // returns the ratio text parsed from views and likes
 const buildRatio = () => {
-  // parse the view count, expected format '93,123 views'
-  const viewsElem = $(viewCountSelector)
-  let viewsText = viewsElem ? viewsElem.innerText.split(' ')[0] : null;
-  if (viewsText === null) return;
-  let views = parseCount(viewsText);
+  const likes = getLikesCount();
+  const views = getViewCount();
+  const ratio = (100.0 * likes / views).toFixed(2);
+  if (isNaN(ratio)) return;
 
-  // Parse the likes count, expected format 'like this video along with 3,239 other people'
-  const likesElem = $(likeButtonSelector);
-  let likesText = likesElem ? likesElem.ariaLabel.split('with ')[1].split(' ')[0] : null;
-  if (likesText === null) return;
-  let likes = parseCount(likesText);
-
-  // create the ratio
-  let ratio = (100.0 * likes / views).toFixed(2);
   let ratioText = `${ratio}% liked`;
   let ratioTooltip = `${fmtNumber(likes)} likes / ${fmtNumber(views)} views = ${ratioText}`;
+  // console.log('YouTube Likes Ratio >> ' + ratioTooltip)
   
-  // update and exit if the element already exists
+  // update and exit if the element already exists...
   const curElem = $(`#${ratioElemId}`);
   if (curElem) {
     curElem.innerText = ratioText;
@@ -81,9 +87,8 @@ const buildRatio = () => {
     return;
   }
 
-  // create new element
-  const anchorElem = $(ratioElemAnchor);
-  if (!anchorElem) throw new Error('Could not find anchor element ' + ratioElemAnchor);
+  // ...or create a new element
+  const containerElem = $(ratioElemContainer);
   const newElem = document.createElement('div');
   newElem.id = ratioElemId;
   newElem.classList.add(
@@ -94,7 +99,7 @@ const buildRatio = () => {
   newElem.style.cssText = 'white-space: nowrap; margin-right: 8px;';
   newElem.innerText = ratioText;
   newElem.title = ratioTooltip;
-  anchorElem.insertAdjacentElement('beforeBegin', newElem);
+  containerElem.insertBefore(newElem, containerElem.firstChild);
 }
 
 // main function
@@ -110,11 +115,11 @@ const parseLikes = () => {
 
 // await elements then parse and set up observers to persist and update the ratio
 const initialize = () => {
-  (elementReady(viewCountSelector) && elementReady(likeButtonSelector) && elementReady(removalObserverSelector)).then(() => {
+  (elementReady(initialViewCountSelector) && elementReady(likeButtonSelector) && elementReady(ratioElemContainer)).then(() => {
     parseLikes();
-    refreshObserver.observe($(viewCountSelector), { attributes: true });
+    refreshObserver.observe($(primaryViewCountSelector), { attributes: true });
     refreshObserver.observe($(likeButtonSelector), { attributes: true });
-    removalObserver.observe($(removalObserverSelector), { childList: true, subtree: true });
+    removalObserver.observe($(ratioElemContainer), { childList: true, subtree: true });
   });
 }
 
