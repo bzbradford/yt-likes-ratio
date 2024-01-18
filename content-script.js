d392148d981dbd679c5a79fade1c229586988c40
Ben Bradford <bradfordium@gmail.com>
2024-01-18 15:56:18 -0600

    simplify code and some fixes
    * don't update ratio when view count changes because the updated like count isn't parsed
    * simplify some functions

--- content-script.js
+++ content-script.js
@@ -5,37 +5,37 @@ let hasNoErrors = true;
 
 // element definitions
 const ratioElemId = 'yt-likes-ratio';
-
-// where to look for view counts, in .innerText (initial) or .ariaLabel (primary)
-const initialViewCountSelector = "#count > ytd-video-view-count-renderer > span.view-count.style-scope.ytd-video-view-count-renderer"
-const primaryViewCountSelector = "#view-count"
-
-// where to look for likes count in .ariaLabel
+const viewCountSelector = "#count > ytd-video-view-count-renderer > span.view-count.style-scope.ytd-video-view-count-renderer";
 const likeButtonSelector = '#top-level-buttons-computed > segmented-like-dislike-button-view-model > yt-smartimation > div > div > like-button-view-model > toggle-button-view-model > button';
-
-// container for ratio element and to trigger re-creating it
 const ratioElemContainer = '#top-level-buttons-computed';
+const ratioElemAnchor = '#top-level-buttons-computed > segmented-like-dislike-button-view-model'
 
 // query selector alias
 const $ = document.querySelector.bind(document);
 
 // await element then resolve promise
-const elementReady = (selector) => {
+const awaitElement = (selector) => {
   return new Promise((resolve) => {
     const el = $(selector);
-    if (el) {
-      resolve(el);
-    } else {
-      const observer = new MutationObserver(() => {
-        const el = $(selector);
-        if (el) {
-          observer.disconnect();
-          resolve(el);
-        }
-      })
-      observer.observe(document.body, { childList: true, subtree: true })
-    }
+    if (el) return resolve(el);
+    const observer = new MutationObserver(() => {
+      const el = $(selector);
+      if (el) {
+        observer.disconnect();
+        resolve(el);
+      }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
   });
+}
+
+// resolve when all selectors present
+const awaitElements = (selectors) => {
+  const promises = [];
+  selectors.forEach(selector => {
+    promises.push(awaitElement(selector));
+  });
+  return Promise.all(promises);
 }
 
 // format number with thousands commas
@@ -51,21 +51,18 @@ const parseCount = (str) => {
   return parseInt(str);
 }
 
-// parse the view count, expected format '93,123 views'
+// parse the view count in .innerText, expected format '93,123 views'
 const getViewCount = () => {
-  const el1 = $(initialViewCountSelector);
-  const el2 = $(primaryViewCountSelector);
-  let viewsText = '';
-  if (el1) viewsText = el1.innerText.split(' ')[0];
-  if (el2 && el2.ariaLabel != '') viewsText = el2.ariaLabel.split(' ')[0];
-  return parseCount(viewsText);
+  const el = $(viewCountSelector);
+  let str = el ? el.innerText.split(' ')[0] : '';
+  return parseCount(str);
 }
 
-// Parse the likes count, expected format 'like this video along with 3,239 other people'
+// Parse the likes count in .ariaLabel, expected format 'like this video along with 3,239 other people'
 const getLikesCount = () => {
   const el = $(likeButtonSelector);
-  let likesText = el ? el.ariaLabel.split('with ')[1].split(' ')[0] : '';
-  return parseCount(likesText);
+  let str = el ? el.ariaLabel.split('with ')[1].split(' ')[0] : '';
+  return parseCount(str);
 }
 
 // returns the ratio text parsed from views and likes
@@ -76,7 +73,7 @@ const buildRatio = () => {
   if (isNaN(ratio)) return;
 
   let ratioText = `${ratio}% liked`;
-  let ratioTooltip = `${fmtNumber(likes)} likes / ${fmtNumber(views)} views = ${ratioText}`;
+  let ratioTooltip = `This video had ${fmtNumber(likes)} likes / ${fmtNumber(views)} views = ${ratioText} when first opened`;
   // console.log('YouTube Likes Ratio >> ' + ratioTooltip)
   
   // update and exit if the element already exists...
@@ -88,7 +85,6 @@ const buildRatio = () => {
   }
 
   // ...or create a new element
-  const containerElem = $(ratioElemContainer);
   const newElem = document.createElement('div');
   newElem.id = ratioElemId;
   newElem.classList.add(
@@ -99,7 +95,8 @@ const buildRatio = () => {
   newElem.style.cssText = 'white-space: nowrap; margin-right: 8px;';
   newElem.innerText = ratioText;
   newElem.title = ratioTooltip;
-  containerElem.insertBefore(newElem, containerElem.firstChild);
+  const anchorElem = $(ratioElemAnchor);
+  anchorElem.insertAdjacentElement('beforebegin', newElem);
 }
 
 // main function
@@ -115,10 +112,9 @@ const parseLikes = () => {
 
 // await elements then parse and set up observers to persist and update the ratio
 const initialize = () => {
-  (elementReady(initialViewCountSelector) && elementReady(likeButtonSelector) && elementReady(ratioElemContainer)).then(() => {
+  const requiredElements = [viewCountSelector, likeButtonSelector, ratioElemContainer, ratioElemAnchor]
+  awaitElements(requiredElements).then(() => {
     parseLikes();
-    refreshObserver.observe($(primaryViewCountSelector), { attributes: true });
-    refreshObserver.observe($(likeButtonSelector), { attributes: true });
     removalObserver.observe($(ratioElemContainer), { childList: true, subtree: true });
   });
 }
@@ -127,11 +123,6 @@ const initialize = () => {
 const initializeObserver = new MutationObserver(() => {
   initialize();
 })
-
-// trigger update on specific page changes
-const refreshObserver = new MutationObserver(() => {
-  parseLikes();
-});
 
 // if the likes element gets removed, regenerate it
 const removalObserver = new MutationObserver((mutations) => {
@@ -143,7 +134,7 @@ const removalObserver = new MutationObserver((mutations) => {
 });
 
 // wait for like button be ready on the page then initialize
-elementReady(likeButtonSelector).then(() => {
+awaitElement(likeButtonSelector).then(() => {
   initializeObserver.observe($('head'), { childList: true, subtree: true });
   initialize();
 });
